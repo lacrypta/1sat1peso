80c4be51e898435bed0b33289db302adc64ab607
Jona <llamasjonat@gmail.com>
2024-03-12 02:04:53 -0300

    fix(component): format value for price on Header and Prices

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -1,35 +1,33 @@
-"use client";
+'use client';
 
-import { Divider, Flex, Heading, Text } from "@lawallet/ui";
-import Image from "next/image";
+import { Divider, Flex, Heading, Text } from '@lawallet/ui';
+import Image from 'next/image';
 
-import { appTheme } from "@/config";
+import { appTheme } from '@/config';
 
-import { Progress } from "../Progress";
+import { Progress } from '../Progress';
 
-import { usePricesContext } from "@/context/PricesContext";
-import { useMemo } from "react";
-import { Background, HeaderPrimitive } from "./style";
+import { usePricesContext } from '@/context/PricesContext';
+import { useMemo } from 'react';
+import { Background, HeaderPrimitive } from './style';
 
 export default function Header() {
   const { pricesData, convertCurrency } = usePricesContext();
 
-  const valueSatInArs = useMemo(
-    () => convertCurrency(1, "SAT", "ARS"),
-    [pricesData]
-  );
+  const valueSatInArs = useMemo(() => convertCurrency(1, 'SAT', 'ARS'), [pricesData]);
+
   return (
     <HeaderPrimitive>
       <Background>
-        <Image src="/map.webp" alt="map" width={2103} height={1248} />
+        <Image src='/map.webp' alt='map' width={2103} height={1248} />
       </Background>
-      <Heading align="center">1SAT = 1PESO</Heading>
+      <Heading align='center'>1SAT = 1PESO</Heading>
       <Flex gap={8}>
         <Progress value={valueSatInArs * 100} />
-        <Text isBold>${valueSatInArs}</Text>
+        <Text isBold>${String(valueSatInArs).replace('.', ',')}</Text>
       </Flex>
       <Divider y={12} />
-      <Text align="center" color={appTheme.colors.gray50}>
+      <Text align='center' color={appTheme.colors.gray50}>
         1 BTC = 100.000.000 SAT
       </Text>
       <Divider y={16} />
--- src/app/components/Prices/index.tsx
+++ src/app/components/Prices/index.tsx
@@ -1,18 +1,22 @@
-import { appTheme } from "@/config";
-import { Flex, Icon, Heading, Text } from "@lawallet/ui";
-import { ArrowUp, ArrowDown } from "../Icons";
-import { Card } from "../Card";
+import { appTheme } from '@/config';
+import { Flex, Icon, Heading, Text } from '@lawallet/ui';
+import { ArrowUp, ArrowDown } from '../Icons';
+import { Card } from '../Card';
 
-import { PricesPrimitive } from "./style";
-import { usePricesContext } from "@/context/PricesContext";
+import { PricesPrimitive } from './style';
+import { usePricesContext } from '@/context/PricesContext';
 
 export function Prices() {
   const { convertCurrency } = usePricesContext();
 
+  function formatNumber(number: any) {
+    return number.replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+  }
+
   return (
     <PricesPrimitive>
       <Card>
-        <Flex align="center" gap={8}>
+        <Flex align='center' gap={8}>
           <Text isBold color={appTheme.colors.gray50}>
             Bitcoin
           </Text>
@@ -23,17 +27,15 @@ export function Prices() {
             <Text color={appTheme.colors.primary}>3.24%</Text>
           </Flex> */}
         </Flex>
-        <Flex align="center" gap={4}>
-          <Heading as="h2">
-            ${convertCurrency(100000000, "SAT", "USD").toFixed(0)}
-          </Heading>
-          <Text size="small" color={appTheme.colors.gray50}>
+        <Flex align='center' gap={4}>
+          <Heading as='h2'>${formatNumber(convertCurrency(100000000, 'SAT', 'USD').toFixed(0))}</Heading>
+          <Text size='small' color={appTheme.colors.gray50}>
             USD
           </Text>
         </Flex>
       </Card>
       <Card>
-        <Flex align="center" gap={8}>
+        <Flex align='center' gap={8}>
           <Text isBold color={appTheme.colors.gray50}>
             Dólar cripto
           </Text>
@@ -44,11 +46,9 @@ export function Prices() {
             <Text color={appTheme.colors.error}>1.49%</Text>
           </Flex> */}
         </Flex>
-        <Flex align="center" gap={4}>
-          <Heading as="h2">
-            ${convertCurrency(1, "USD", "ARS").toFixed(0)}
-          </Heading>
-          <Text size="small" color={appTheme.colors.gray50}>
+        <Flex align='center' gap={4}>
+          <Heading as='h2'>${formatNumber(convertCurrency(1, 'USD', 'ARS').toFixed(0))}</Heading>
+          <Text size='small' color={appTheme.colors.gray50}>
             ARS
           </Text>
         </Flex>
